efa3867bebdd3a31f18c23bac7156fd4d3997e1e
mateusnovo <mpnmateus@gmail.com>
2024-10-03 16:20:47 -0300

    [refactor]: alterando nome da classe que gerencia beans

--- src/main/java/springboot_starter_app/conversorcep/BeanFactory.java
+++ src/main/java/springboot_starter_app/conversorcep/BeanFactory.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Beans {
+public class BeanFactory {
     @Bean
     public Gson gson(){
         return new Gson();
