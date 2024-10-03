7ceee767eaf162d5e38235758c7b9f3b9d3ed997
mateusnovo <mpnmateus@gmail.com>
2024-10-03 18:37:49 -0300

    refactor: corrigindo CommandLineRunner -> throws Exception

--- src/main/java/springboot_starter_app/sistemamensagens/SpringBootApplicationMensagem.java
+++ src/main/java/springboot_starter_app/sistemamensagens/SpringBootApplicationMensagem.java
@@ -4,13 +4,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springboot_starter_app.conversorcep.SpringBootApplicationCep;
 
 public class SpringBootApplicationMensagem {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplicationMensagem.class, args);
     }
     @Bean
-    public CommandLineRunner run(SistemaMensagem sistema) {
+    public CommandLineRunner run(SistemaMensagem sistema) throws Exception {
         return args -> {
             sistema.enviarConfirmacaoCadastro();
             sistema.enviarMensagemBoasVindas();
