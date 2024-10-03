917bbc78a414d662f151c1d2657f88fa56306c72
mateusnovo <mpnmateus@gmail.com>
2024-10-03 18:29:13 -0300

    feat: incluindo um bean para criaÃcao de um objeto, com seus atributos setados

--- src/main/java/springboot_starter_app/sistemamensagens/BeansFactory.java
+++ src/main/java/springboot_starter_app/sistemamensagens/BeansFactory.java
@@ -1,4 +1,16 @@
 package springboot_starter_app.sistemamensagens;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeansFactory {
+    @Bean
+    public Remetente remetente (){
+        System.out.println("CRIANDO UM OBJETO REMETENTE");
+        Remetente remetente = new Remetente();
+        remetente.setEmail("noreply@dio.com.br");
+        remetente.setNome("Tech Elite");
+        return remetente;
+    }
 }
