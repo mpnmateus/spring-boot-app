ae53624ae3420173f0e52ec3364148b8811f2976
mateusnovo <mpnmateus@gmail.com>
2024-10-03 16:04:42 -0300

    [feat]: usando um bean na classe de inicializacao do container springboot run.

---
+++ src/main/java/springboot_starter_app/conversorcep/SpringBootApplicationCep.java
@@ -0,0 +1,22 @@
+package springboot_starter_app.conversorcep;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springboot_starter_app.calculadora.SpringBootAppApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationCep {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner run(ConversorJson conversor) throws Exception {
+        return args -> {
+            String json = "{\"cep\": \"01001-000\", \"logradouro\": \"Praça da Sé\", \"Localidade\": \"POA\"}";
+            ViaCepResponse response = conversor.converter(json);
+            System.out.println("Dados do CEP: " + response);
+        };
+    }
+}
