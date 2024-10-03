3e181906d51581c903574563d259c219b23a5481
mateusnovo <mpnmateus@gmail.com>
2024-10-03 16:08:00 -0300

    feat: classe apenas para configuracoes @bean

---
+++ src/main/java/springboot_starter_app/conversorcep/Beans.java
@@ -0,0 +1,13 @@
+package springboot_starter_app.conversorcep;
+
+import com.google.gson.Gson;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Beans {
+    @Bean
+    public Gson gson(){
+        return new Gson();
+    }
+}
