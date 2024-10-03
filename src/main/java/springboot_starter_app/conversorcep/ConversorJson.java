c0420385debbae61e12896c99f2525abee9f0b1a
mateusnovo <mpnmateus@gmail.com>
2024-10-03 16:05:58 -0300

    Refactor: usando anotacao @Autowired para usar o componente Gson

--- src/main/java/springboot_starter_app/conversorcep/ConversorJson.java
+++ src/main/java/springboot_starter_app/conversorcep/ConversorJson.java
@@ -1,10 +1,12 @@
 package springboot_starter_app.conversorcep;
 
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConversorJson {
+    @Autowired
     private Gson gson;
     public ViaCepResponse converter (String json){
         ViaCepResponse response = gson.fromJson(json, ViaCepResponse.class);
